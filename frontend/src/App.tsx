a6fd9a37b0850bd0c53dd114848aa73a3c140e2e
MinJune Kim <qria@qria.net>
2022-10-26 17:23:17 +0900

    change eslint setting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 
 function App() {
+  const [message, setMessage] = React.useState('Hello World');
   return (
     <div className="bg-white">
       <div className="mx-auto">
         <h1 className="text-3xl font-bold underline">
-          Hello World!
+          {process.env.REACT_APP_KAKAO_API_KEY}
         </h1>
       </div>
     </div>
