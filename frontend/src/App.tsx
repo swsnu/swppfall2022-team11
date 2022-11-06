ad0ef1d0e505eed1366f47afb89dab9f254bd19b
juhwancho <49906112+juhwancho@users.noreply.github.com>
2022-11-06 12:40:32 +0900

    resolute conflict

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,7 +11,6 @@ import DecoratePage from './Pages/DecorateLetterPage'
 function App() {
   const [message, setMessage] = React.useState('Hello World');
   return (
-<<<<<<< HEAD
     <Routes>
     <Route path='/main' element={<MainPage></MainPage>}>  </Route>  
     <Route path="/" element={<Navigate replace to={"/main"} />} />
@@ -19,15 +18,6 @@ function App() {
     <Route path="/gift" element={<GiftPage></GiftPage>} > </Route> 
     <Route path="/decorate" element={<DecoratePage></DecoratePage>} > </Route>   
     </Routes>
-=======
-    <div className="bg-white">
-      <div className="mx-auto">
-        <h1 className="text-3xl font-bold underline">
-          {process.env.REACT_APP_KAKAO_API_KEY}
-        </h1>
-      </div>
-    </div>
->>>>>>> a6fd9a37b0850bd0c53dd114848aa73a3c140e2e
   );
 }
 
