24783bbf4ac2616380780fb6b678cb84f038956a
MinJune Kim <qria@qria.net>
2022-11-06 13:59:07 +0900

    mainpage

--- frontend/src/Pages/MainPage.tsx
+++ frontend/src/Pages/MainPage.tsx
@@ -1,19 +1,29 @@
-import { useNavigate } from "react-router-dom";
 import Navigation from "../components/Navigation";
 export default function MainPage() {
-
-  const navigate = useNavigate();
   return (
     <>
       <Navigation />
-      <main className="w-full h-full">
-        <div className="relative w-full h-full">
-          <div className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              'backgroundImage': "url(/background.jpg)"
-            }}>
-            <span className="w-full h-full absolute opacity-75 bg-black"></span>
+      <main className="relative w-full h-full">
+        <div className="absolute top-0 w-full h-full bg-center bg-cover"
+          style={{
+            'backgroundImage': "url(/background.jpg)"
+          }}>
+          <span className="w-full h-full absolute opacity-60 bg-black"></span>
+        </div>
+
+        <div className="relative flex flex-col items-center justify-center h-full pb-10">
+          <div className="w-full px-4 text-center">
+            <h1 className="text-gray-200 font-semibold text-xl lg:text-5xl">
+              AI 큐피드가
+            </h1>
+            <h1 className="text-gray-200 font-semibold text-xl lg:text-5xl mt-2 lg:mt-4">
+              당신의 연애를 성공시켜드립니다
+            </h1>
+            <p className="mt-2 lg:mt-4 text-lg lg:text-2xl text-gray-200">
+              AI로 감동적인 연애편지를 써보세요.
+            </p>
           </div>
+          <a href="/create" className="rounded-xl text-lg lg:text-3xl p-4 lg:p-8 bg-gray-200 mt-20 text-gray-600 font-semibold">지금 시작하기</a>
         </div>
       </main>
     </>
