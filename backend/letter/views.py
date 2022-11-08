1baee01ebc5d8300d00259597941886015f517a8
MinJune Kim <qria@qria.net>
2022-11-08 15:39:52 +0900

    add backend generate mock api

--- backend/letter/views.py
+++ backend/letter/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+import json
+from json.decoder import JSONDecodeError
+
+from django.http import JsonResponse, HttpResponseBadRequest, HttpResponseNotAllowed
+from django.views.decorators.csrf import csrf_exempt
 
 def index(request):
     return JsonResponse({'message': 'OK'})
+
+@csrf_exempt
+def generate(request):
+    if request.method == 'POST':
+        try:
+            body = request.body.decode()
+            letter_type = json.loads(body)['letterType']
+            feel = json.loads(body)['feel']
+            voice = json.loads(body)['voice']
+        except (KeyError, JSONDecodeError) as e:
+            return HttpResponseBadRequest()
+        response_dict = {'generatedText': 'generatedText', 'letterType': letter_type, 'feel': feel, 'voice': voice}
+        return JsonResponse(response_dict, status=201)
+    else:
+        return HttpResponseNotAllowed(['POST'])
--- backend/letter_of_cupid/settings.py
+++ backend/letter_of_cupid/settings.py
@@ -87,7 +87,7 @@ WSGI_APPLICATION = 'letter_of_cupid.wsgi.application'
 
 DATABASES = {
     'default':  dj_database_url.config(    
-         default='sqlite:////./db.sqlite3',
+         default='sqlite:///./db.sqlite3',
          conn_max_age=600    
     )
 }
