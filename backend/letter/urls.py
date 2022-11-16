d5c7d3d10695fa213a6ddbcc9cbd22104c0e1331
MinJune Kim <qria@qria.net>
2022-11-16 12:57:51 +0900

    add backend test

--- backend/letter/urls.py
+++ backend/letter/urls.py
@@ -5,5 +5,4 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('generate', views.generate, name='generate'),
-    path('debug', views.debug, name='debug'),
 ]
