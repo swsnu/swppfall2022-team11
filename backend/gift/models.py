b0fb7790edc13bc65ca786607f1b5c226eb35b5e
Namhyeon Heo <nammath11@gmail.com>
2022-11-24 01:09:04 +0900

    gift backend

--- backend/gift/models.py
+++ backend/gift/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Gift(models.Model):
+    name = models.CharField(default="", max_length=64)
+    price = models.IntegerField(default=0)
+    link = models.URLField(default="", max_length=400)
+    img = models.URLField(default="", max_length=400)
+
+    def __str__(self):
+        return self.name
--- backend/gift/views.py
+++ backend/gift/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Gift
 
 def index(request):
     return JsonResponse({'message': 'GIFT OK'})
+
+@csrf_exempt
+def gift_list(request):
+    if request.method == 'GET':
+        gift_all_list = [gift for gift in Gift.objects.all().values()]
+        return JsonResponse(gift_all_list, safe=False)
