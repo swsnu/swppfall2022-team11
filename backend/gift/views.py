23e2dc54e6189ebc963f0fe68d8ddf35bfa9b13f
Namhyeon Heo <nammath11@gmail.com>
2022-12-14 02:48:27 +0900

    change recommend

--- backend/gift/views.py
+++ backend/gift/views.py
@@ -5,13 +5,56 @@ from .models import Gift
 import requests
 from bs4 import BeautifulSoup 
 import json
+import random
 
 
 @csrf_exempt
 def gift_list(request):
     if request.method == 'GET':
-        gift_all_list = [gift for gift in Gift.objects.all().values()]
-        return JsonResponse(gift_all_list, safe=False)
+        #body = request.body.decode()
+        #body = eval(body)
+        #section = body['section']
+        url = 'https://m.shopping.naver.com/gift/plans/641486?NaPm=ct%3Dlbmgravk%7Cci%3Dshoppingwindow%7Ctr%3Dgifth%7Chk%3D0da8bda02a17df2f2d763cf090eb772e320b6174%7Ctrx%3D&first=6052509554'
+        url1 = 'https://m.shopping.naver.com/gift/plans/641486?first=7681567774&NaPm=ct%3Dlbmie4e0%7Cci%3Dshoppingwindow%7Ctr%3Dgifth%7Chk%3De49a2edb77689348a8eb9bbfe1b514212350de02%7Ctrx%3D'
+        url2 = 'https://m.shopping.naver.com/gift/plans/641486?first=5866760828&NaPm=ct%3Dlbmigf84%7Cci%3Dshoppingwindow%7Ctr%3Dgifth%7Chk%3Dd4dcd75040c1f2b65fcd1fe82ae7bfccb28efbb3%7Ctrx%3D'
+        headers = { 'Accept-Language' : 'ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7,zh-TW;q=0.6,zh;q=0.5',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36',
+            'Accept-Encoding': 'gzip'
+        }
+        raw = requests.get(url=url, headers=headers)
+        html = BeautifulSoup(raw.text, 'html.parser')
+        gift = []
+        for i in range(2, 61):
+            img = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div.gp3TgRTw_E > img')['src']
+            price = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div._1OqXul1sMG > div > span').get_text()
+            name = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div._1OqXul1sMG > strong').get_text()
+            price = int(price.replace(',',''))
+            link = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > a')['href']
+            data = {"img":img, "name":name, "price":price, "link":link}
+            gift.append(data)
+        raw = requests.get(url=url1, headers=headers)
+        html = BeautifulSoup(raw.text, 'html.parser')
+        for i in range(2, 21):
+            img = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div.gp3TgRTw_E > img')['src']
+            price = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div._1OqXul1sMG > div > span').get_text()
+            name = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div._1OqXul1sMG > strong').get_text()
+            price = int(price.replace(',',''))
+            link = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > a')['href']
+            data = {"img":img, "name":name, "price":price, "link":link}
+            gift.append(data)
+        raw = requests.get(url=url2, headers=headers)
+        html = BeautifulSoup(raw.text, 'html.parser')
+        for i in range(2, 21):
+            img = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div.gp3TgRTw_E > img')['src']
+            price = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div._1OqXul1sMG > div > span').get_text()
+            name = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > div._1OqXul1sMG > strong').get_text()
+            price = int(price.replace(',',''))
+            link = html.select_one(f'#content > div > div._1rlmqfkmSs > div > ul > li:nth-child({i}) > div > a')['href']
+            data = {"img":img, "name":name, "price":price, "link":link}
+            gift.append(data)        
+
+        
+        return JsonResponse(gift, safe=False)
 
 
 def shop_keyword(request):
